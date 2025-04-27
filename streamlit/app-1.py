d584a5b91731486f8e7529bae792689b6742c271
Bishnu <thapabishnu20@gmail.com>
2025-04-27 08:11:19 +0545

    first streamlit project with basics

---
+++ streamlit/app-1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("Bishnu Thapa")
+st.write("This is my first Streamlit app.")
+# st.button("Click me!")
+if st.button("Click me!"):
+    st.success("Button clicked!")
